afb81213df74b2c197c6868665f527c5bfcc05e7
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-05-20 15:02:19 +0400

    first commit

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,59 @@
+import UserCard from "@/components/cards/UserCard";
+import Pagination from "@/components/shared/Pagination";
+import SearchBar from "@/components/shared/SearchBar";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React, { FC } from "react";
+
+const Page: FC<{
+  searchParams: { [key: string]: string | undefined };
+}> = async ({ searchParams }) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      <SearchBar routeType="search" />
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </section>
+  );
+};
+
+export default Page;
